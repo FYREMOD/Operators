17a7a5c30bfab820df227ff07362db579c437cc2
LivingInTarkov <66750391+FyreModServer@users.noreply.github.com>
2023-09-10 07:16:10 -0600

    Create properties.cs

---
+++ properties.cs
@@ -0,0 +1,7 @@
+[System.Serializable]
+public class AmmunitionType[] = [pistol, shotgun, rifle, other]
+{
+    public string name;
+    public float bulletMassInGrains;
+    public float bulletVelocityInFps;
+}
